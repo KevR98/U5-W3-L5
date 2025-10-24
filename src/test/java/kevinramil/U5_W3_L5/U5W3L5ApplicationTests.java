194d0c32ff95d3ac25cc4bb2d7f2459dcc644b28
KevR98 <ramil.kevin98@gmail.com>
2025-10-24 10:30:02 +0200

    Initial commit

---
+++ src/test/java/kevinramil/U5_W3_L5/U5W3L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package kevinramil.U5_W3_L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
